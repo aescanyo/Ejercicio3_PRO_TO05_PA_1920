1f2d892c2e7be9f9981370b98e0cacab96d49af8
antonio <antonio@Hyperion.localdomain>
2021-03-04 20:11:19 +0100

    Commit en local

---
+++ src/Ejercicio3_pro_to05_pa_1920/Ejercicio3_PRO_TO05_PA_1920.java
@@ -0,0 +1,18 @@
+package Ejercicio3_pro_to05_pa_1920;
+
+
+
+/**
+ *
+ * @author antonio
+ */
+public class Ejercicio3_PRO_TO05_PA_1920 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/Ejercicio3_pro_to05_pa_1920/Patinete.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ejercicio3_pro_to05_pa_1920;
+
+/**
+ *
+ * @author antonio
+ */
+public class Patinete {
+    
+}
